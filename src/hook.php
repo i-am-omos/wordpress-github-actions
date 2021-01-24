dd8d1bea5934e828f1de337706d7ffae4a40d3ca
RenOmori <omori.r95@gmail.com>
2021-01-24 17:52:40 +0900

    fix: replate publish_post hook that is called twice to transition_post_status that manage each transtion of status.

--- src/hook.php
+++ src/hook.php
@@ -2,28 +2,31 @@
 
 function push_repository_dispatch_event($post_id, $post)
 {
-    if ($post->post_status === 'publish') {
-        $data = "'{" . '"event_type": "' . esc_attr(get_option('github_event_type')) . '"}' . "'";
-        $url = 'https://api.github.com/repos/' . esc_attr(get_option('github_account')) . '/' . esc_attr(get_option('github_repository')) . '/dispatches';
-        $token = esc_attr(get_option('github_token'));
+  $data = "'{" . '"event_type": "' . esc_attr(get_option('github_event_type')) . '"}' . "'";
+  $url = 'https://api.github.com/repos/' . esc_attr(get_option('github_account')) . '/' . esc_attr(get_option('github_repository')) . '/dispatches';
+  $token = esc_attr(get_option('github_token'));
 
-        exec("curl -H \"Authorization: token ${token}\" -H \"Accept: application/vnd.github.everest-preview+json\" \"${url}\" -d ${data}");
-    }
+  exec("curl -H \"Authorization: token ${token}\" -H \"Accept: application/vnd.github.everest-preview+json\" \"${url}\" -d ${data}");
 }
 
-function webhook_future_post($post_id)
+function webhook_transition_post_status($new_status, $old_status, $post)
 {
-    push_repository_dispatch_event($post_id, get_post($post_id));
+  if( $old_status != 'publish' && $new_status == 'publish' ) {
+    push_repository_dispatch_event($post->ID, $post);
+  }
+  if( $old_status == 'publish' && $new_status == 'draft' ) {
+    push_repository_dispatch_event($post->ID, $post);
+  }
+  if( $old_status == 'publish' && $new_status == 'private' ) {
+    push_repository_dispatch_event($post->ID, $post);
+  }
+  if( $old_status == 'publish' && $new_status == 'pending' ) {
+    push_repository_dispatch_event($post->ID, $post);
+  }
+  if( $old_status == 'publish' && $new_status == 'trash' ) {
+    push_repository_dispatch_event($post->ID, $post);
+  }
 }
 
-function webhook_update($post_id, $post_after, $post_before)
-{
-    push_repository_dispatch_event($post_id, $post_after);
-}
-
-add_action('publish_future_post', 'webhook_future_post', 10, 1);
-add_action('publish_post', 'push_repository_dispatch_event', 10, 2);
-add_action('post_updated', 'webhook_update', 10, 3);
-add_action('deleted_post', 'push_repository_dispatch_event', 10, 2);
-
+add_action('transition_post_status', 'webhook_transition_post_status', 10, 3);
 ?>
